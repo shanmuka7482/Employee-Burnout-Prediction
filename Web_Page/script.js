886cf48607d0172f2f9e8303f59f0319fdba042b
Shanmuka Srinivas <shanmuka srinivasa>
2024-07-29 13:21:01 +0530

    changed the address of api

--- Web_Page/script.js
+++ Web_Page/script.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 
     $.ajax({
       type: "POST",
-      url: "http://127.0.0.1:5000/submit",
+      url: "https://employee-burnout-prediction-backend.onrender.com/submit",
       data: formData,
       success: function (response) {
         $("#ans").html("<h2>Ans: " + response + "%</h2>");
